51f44e6569d6297cb5e95a088aa3a96c93265611
ferrari-nico-edu-acc <ferrari.nico@iisgalvanimi.edu.it>
2026-01-30 09:00:07 +0000

    punto 4

--- es1/script.js
+++ es1/script.js
@@ -6,6 +6,8 @@ const warm_button = document.querySelector("#warm_button");
 const cold_button = document.querySelector("#cold_button");
 /** @type {HTMLButtonElement} */
 const rgb_button = document.querySelector("#rgb_button");
+/** @type {HTMLInputElement} */
+const opacity_slider = document.querySelector("#opacity_slider");
 
 let last_color = "warm";
 
@@ -18,3 +20,11 @@ function change_color_to(to) {
 warm_button.addEventListener("click", () => change_color_to("warm"));
 cold_button.addEventListener("click", () => change_color_to("cold"));
 rgb_button.addEventListener("click", () => change_color_to("rgb"));
+
+opacity_slider.addEventListener("input",() => {
+    const opacity = Number(opacity_slider.value);
+    if (!isFinite(opacity)) {
+        return;
+    }
+    lamp.style.opacity = `${opacity/100}`
+})
